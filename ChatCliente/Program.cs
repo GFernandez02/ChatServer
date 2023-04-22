1c97b71fa2f43caac564fda3f04724d30a898288
GFernandez <GFer@gmail.com>
2023-04-22 11:35:18 -0400

    Se realizo conexión cliente con el servidor

--- ChatCliente/Program.cs
+++ ChatCliente/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ChatCliente
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatCliente
 {
     internal class Program
     {
+        static string alias = "usuario nuevo";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            int port = 5000;
+            TcpClient cliente = new TcpClient();
+            cliente.Connect(ip, port);
+            Console.WriteLine("Se conectó cliente");
+            string s = Console.ReadLine();
         }
     }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace ChatServer
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
 {
     internal class Program
     {
+        static object _bloqueo=new object();
+        static int contador = 1;
+
+        static Dictionary<int, TcpClient> clientes=new Dictionary<int, TcpClient>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo");
+
+            TcpListener listener= new TcpListener(IPAddress.Any,5000);
+            listener.Start();
+            Console.WriteLine("Escuchando puerto 5000");
+
+            while (true)
+            {
+                TcpClient client= listener.AcceptTcpClient();
+
+                lock(_bloqueo) {
+                    clientes.Add(contador, client);
+                }
+                Console.WriteLine("{0} Se conectó", client.Client.RemoteEndPoint.ToString());
+            }
         }
     }
 }
