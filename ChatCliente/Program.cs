d66ca90308d2d4b1d7c2f0d784a6f32e3c29c93d
GFernandez <GFer@gmail.com>
2023-04-22 12:36:05 -0400

    Se realizó la implementación de mensajes entre varios usuarios

--- ChatCliente/Program.cs
+++ ChatCliente/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace ChatCliente
 {
@@ -8,12 +9,41 @@ namespace ChatCliente
         static string alias = "usuario nuevo";
         static void Main(string[] args)
         {
+            Console.Write("Ingrese su alias: ");
+            alias = Console.ReadLine();
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             int port = 5000;
             TcpClient cliente = new TcpClient();
             cliente.Connect(ip, port);
             Console.WriteLine("Se conectó cliente");
-            string s = Console.ReadLine();
+            NetworkStream stream = cliente.GetStream();
+            Thread t = new Thread((cliente) => RecibirDatos((TcpClient)cliente));
+            t.Start(cliente);
+            string mensaje;
+            while (!string.IsNullOrEmpty((mensaje = Console.ReadLine())))
+            {
+                mensaje = string.Concat(alias," : " , mensaje);
+                byte[] buffer = Encoding.ASCII.GetBytes(mensaje);
+                stream.Write(buffer, 0, buffer.Length);
+
+            }
+            cliente.Client.Shutdown(SocketShutdown.Send);
+            t.Join();
+            stream.Close();
+            cliente.Close();
+            Console.ReadKey();
+        }
+
+        private static void RecibirDatos(TcpClient cliente)
+        {
+            NetworkStream stream = cliente.GetStream();
+            byte[] bytesRecibidos = new byte[1024];
+            int byteCount;
+
+            while ((byteCount = stream.Read(bytesRecibidos, 0, bytesRecibidos.Length)) > 0)
+            {
+                Console.Write(Encoding.ASCII.GetString(bytesRecibidos, 0, byteCount));
+            }
         }
     }
 }
