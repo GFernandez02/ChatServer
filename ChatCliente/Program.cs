59eb5692aa88f590da04c97fee32541f4f41a00b
GFernandez <guillermo.fernandez02@inacapmail.cl>
2023-05-06 10:40:42 -0400

    Se ha limpiado y documentado el codigo para mayor entendimiento de su uso

--- ChatCliente/Program.cs
+++ ChatCliente/Program.cs
@@ -1,25 +1,35 @@
-﻿using System.Net;
+﻿//Se implementan paquetes de datos
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+//Se inicia el proceso del programa del cliente para el servidor
 namespace ChatCliente
 {
+    //Se crea la clase interna del programa    
     internal class Program
     {
+        //Se crea un metodo para seleccionar un nombre de usuario
         static string alias = "usuario nuevo";
         static void Main(string[] args)
         {
+            //Se da la opcion de escribir un alias al gusto del usuario para implementarlo en el servidor
             Console.Write("Ingrese su alias: ");
             alias = Console.ReadLine();
             IPAddress ip = IPAddress.Parse("127.0.0.1");
+            //Se abre un puerto determinando donde se alojará este servidor
             int port = 5000;
             TcpClient cliente = new TcpClient();
+            //Se realiza la prueba del metodo, para asi lograr una conexion local con el servidor en el puerto 5000
             cliente.Connect(ip, port);
+            //Se conecta sin problemas al puerto reservado para abrir servidor
             Console.WriteLine("Se conectó cliente");
+            //Con este metodo, se toman los datos del flujo del cliente para que este pueda empezar a enviar un mensaje
             NetworkStream stream = cliente.GetStream();
             Thread t = new Thread((cliente) => RecibirDatos((TcpClient)cliente));
             t.Start(cliente);
             string mensaje;
+            //Se da a entender que mientras el cliente envie un mensaje este debe estar con un dato para ser enviado, de lo contrario se termina esta funcion
             while (!string.IsNullOrEmpty((mensaje = Console.ReadLine())))
             {
                 mensaje = string.Concat(alias," : " , mensaje);
@@ -36,10 +46,11 @@ namespace ChatCliente
 
         private static void RecibirDatos(TcpClient cliente)
         {
+            //Se crea el metodo para poder recibir los datos del cliente hacia el servidor
             NetworkStream stream = cliente.GetStream();
             byte[] bytesRecibidos = new byte[1024];
             int byteCount;
-
+            //Mientras el mensaje a enviar esté en codigo ASCII y su valor sea mayor a 0, este se enviará
             while ((byteCount = stream.Read(bytesRecibidos, 0, bytesRecibidos.Length)) > 0)
             {
                 Console.Write(Encoding.ASCII.GetString(bytesRecibidos, 0, byteCount));
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,23 +1,29 @@
-﻿using System.Net;
+﻿//Se importan los paquetes de datos
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+//Se inicia el proceso del servidor para abrir puertos y alojar un servidor local
 namespace ChatServer
 {
     internal class Program
     {
+        //Se declaran los objets para poder realizar la conexion con el puerto en un servidor local
         static object _bloqueo=new object();
         static int contador = 1;
 
+        //Se accede al diccionario para poder declarar un TCP/IP el cual, se usara para abrir un puerto local
         static Dictionary<int, TcpClient> clientes=new Dictionary<int, TcpClient>();
 
         static void Main(string[] args)
         {
 
-            TcpListener listener= new TcpListener(IPAddress.Any,5000);
+            //El TCP utiliza el puerto 5000 para poder realizar cualquier conexión
+            TcpListener listener = new TcpListener(IPAddress.Any,5000);
             listener.Start();
             Console.WriteLine("Escuchando puerto 5000");
 
+            //Mientras el TCP de client escuche al TCP de cliente este le asigna una id al alias creado y si es exitoso se muestra un mensaje en pantalla
             while (true)
             {
                 TcpClient client= listener.AcceptTcpClient();
@@ -25,6 +31,7 @@ namespace ChatServer
                 lock(_bloqueo) {
                     clientes.Add(contador, client);
                 }
+                //Una vez el TCP encuentra conexion con el alias, este aparece en el servidor conectado, con su id respectivamente
                 Console.WriteLine("{0} Se conectó", client.Client.RemoteEndPoint.ToString());
                 Thread t = new Thread(ManejarClientes); 
                 t.Start(contador);
@@ -34,6 +41,7 @@ namespace ChatServer
 
         private static void ManejarClientes(object obj)
         {
+            //Se implementa el metodo para poder identificar a cada cuenta con una id y la condición para que pueda enviar un mensaje
             int id = (int)obj;
             TcpClient cliente;
             lock(_bloqueo)
@@ -44,12 +52,13 @@ namespace ChatServer
             }
             while (true)
             {
+                //Se implementa NetworkStream para poder declarar en bytes la ram a utilizar de este programa
                 NetworkStream stream = cliente.GetStream();
                 byte[] buffer = new byte[1024];
                 int byteCount = stream.Read(buffer, 0, buffer.Length);
 
                 if (byteCount == 0) break;
-
+                //Se abre un string que va a codificar los mensajes por UTF-8 y, el usuario podrá enviar mensajes
                 string data = Encoding.UTF8.GetString(buffer, 0, byteCount);
 
                 EnviarMensajes(data);
